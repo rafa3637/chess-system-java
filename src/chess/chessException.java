cbfa9047a7cb33f27e8e5214ac349b877ba3c5a3
rafa3637 <rafaoliverpi@gmail.com>
2022-01-18 11:37:05 -0300

    Implementing possible moves of Rook

--- src/chess/chessException.java
+++ src/chess/chessException.java
@@ -2,7 +2,8 @@ package chess;
 
 public class ChessException extends RuntimeException {
 
-	private static final long serailVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+	
 	
 	public ChessException(String msg){
 		super(msg);
